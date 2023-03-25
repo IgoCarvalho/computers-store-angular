384b2a253a41e530ad64ea0c8af6cacd631a5347
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-25 09:15:35 -0300

    ✨ create products module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', redirectTo: 'produtos', pathMatch: 'full'},
+  { path: 'produtos', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+]
 
 @NgModule({
   declarations: [],
