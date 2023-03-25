874ae206a4f619bb1d068b77d7b78f8688ab916d
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-25 11:42:30 -0300

    ✨ create product details page

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
